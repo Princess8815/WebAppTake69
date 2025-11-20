62010854c7a094ef8b041a6db4447ff801eefb32
PrincessTG8815 <transgirl8815@yahoo.com>
2025-11-20 17:43:42 -0600

    Attempt to add all files

--- IntegrationTest/obj/Debug/net8.0/IntegrationTest.AssemblyInfo.cs
+++ IntegrationTest/obj/Debug/net8.0/IntegrationTest.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IntegrationTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+645b082e5a103c1b0763d72832e45ad40658ded1")]
 [assembly: System.Reflection.AssemblyProductAttribute("IntegrationTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IntegrationTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- YourProject.tests/obj/Release/net8.0/YourProject.tests.AssemblyInfo.cs
+++ YourProject.tests/obj/Release/net8.0/YourProject.tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("YourProject.tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+645b082e5a103c1b0763d72832e45ad40658ded1")]
 [assembly: System.Reflection.AssemblyProductAttribute("YourProject.tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("YourProject.tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
